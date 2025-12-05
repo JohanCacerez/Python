ac8e9d540a55a9b482c86f33aed04af42b2bcbb8
JohanCacerez <johancacerez10@gmail.com>
2025-12-04 21:00:56 -0600

    concurrencia: descarga de urls con Pool

--- Concurrencia/DownloadURL.py
+++ Concurrencia/DownloadURL.py
@@ -8,12 +8,6 @@ b) ThreadPoolExecutor
 c) asyncio + aiohttp
 '''
 
-import threading
-import requests
-
-#En escenarios reales es mejor proteger la escritura en la lista compartida
-lock = threading.Lock()
-
 # Generamos una lista con los url a descargar
 urls = [
     'https://www.example.com',
@@ -24,6 +18,13 @@ urls = [
     'https://www.wikipedia.org',
     'https://www.medium.com',
     'https://www.linkedin.com',]
+
+'''
+import threading
+import requests
+
+#En escenarios reales es mejor proteger la escritura en la lista compartida
+lock = threading.Lock()
 
 #Creamos una lista donde se almacenaran los objetos Thread
 threads = []
@@ -55,9 +56,32 @@ for thread in threads:
     thread.join()   
 print("Descarga con threading.Thread finalizada.")
 
-print("Descargados: ", threads)
+print("Descargados: ", len(results))
+'''
 
+'''
+Un Pool nos sirve para poder manejar de mejor manera multiples hilos sin tener que crearlos y manejarlos, ya que estos los crea
+de manera autoamtica en el Pool
+'''
+'''
+from concurrent.futures import ThreadPoolExecutor
+import requests
 
+results = []
 
+def worker(url):
+    content = fetch_url(url)
+    results.append((url, content))
 
+def fetch_url(url):
+    response = requests.get(url)
+    return response.text
+with ThreadPoolExecutor(max_workers=len(urls)) as executor:
+    for url in urls:
+        print(f"Iniciando descarga de {url} con Pool") 
+        executor.submit(worker, url)
 
+print("Descarga con Pool finalizada.")
+
+print("Descargados: ", len(results))
+'''
